9149568397eac2a03040e4063cef17c8da21b684
Hiroya Shitogiden <dende.capo@gmail.com>
2022-10-06 12:05:18 +0900

    spabase関連コメント化

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,17 +1,19 @@
+//supabaseのutill
+
 import { createClient } from "@supabase/supabase-js";
 
-const getSupabase = (access_token: string) => {
-	const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
+// const getSupabase = (access_token: string) => {
+// 	const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
 
-	if (access_token) {
-		supabase.auth.session = () => ({
-			token_type: "",
-			access_token,
-			user: null
-		});
-	}
+// 	if (access_token) {
+// 		supabase.auth.session = () => ({
+// 			token_type: "",
+// 			access_token,
+// 			user: null
+// 		});
+// 	}
 
-	return supabase;
-};
+// 	return supabase;
+// };
 
-export { getSupabase };
+// export { getSupabase };
